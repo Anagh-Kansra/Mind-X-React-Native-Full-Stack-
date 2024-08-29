a1251a7c95d3a3eb4581f04531e52184f8fe6367
Anagh Kansra <kansraanagh1r@gmail.com>
2024-08-29 11:08:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -31,6 +31,13 @@ app.use(morgan("dev"));
 app.use("/api/v1/auth", require("./routes/userRoutes"));
 app.use("/api/v1/post", require("./routes/postRoutes"));
 
+app.get("/",(req,res) => {
+  res.status(200).send({
+    success:true,
+    message: "Node Server Running...",
+});
+});
+
 // PORT
 const PORT = process.env.PORT || 8080;
 
